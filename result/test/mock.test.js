cad88c5b3e58fc1f544eba0ba208e44a84bfb287
Philipp Gorski <pgo@sysgo.com>
2023-01-06 21:16:49 +0100

    adding mock test

--- result/test/mock.test.js
+++ result/test/mock.test.js
@@ -1,29 +1,33 @@
 const expect = require('chai').expect;
 
 describe('mock test 1', () => {
-  it('unit test 1', () => {
-    expect(true).to.be.true;
-  });
-}); 
+    it('unit test 1', () => {
+        expect(true).to.be.true;
+    });
+});
 
 
 describe('mock test 2', () => {
-  it('unit test 2', () => {
-    expect(true).to.be.true;
-  });
+    it('unit test 2', () => {
+        expect(true).to.be.true;
+    });
 });
 
 describe('mock test 3', () => {
-  it('unit test 3', () => {
-    expect(true).to.be.true;
-  });
+    it('unit test 3', () => {
+        expect(true).to.be.true;
+    });
 });
 
 
 describe('mock test 4', () => {
-  it('unit test 4', () => {
-    expect(true).to.be.true;
-  });
+    it('unit test 4', () => {
+        expect(true).to.be.true;
+    });
 });
 
-
+describe('mock test 5', () => {
+    it('unit test 5', () => {
+        expect(true).to.be.true;
+    });
+});
