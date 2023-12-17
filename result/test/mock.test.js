b253414fa2bbf18e6abbed5e8c798a2905878028
Oleksii Pavliuk <capetruha@gmail.com>
2023-12-18 08:36:56 +1100

    added more tests

--- result/test/mock.test.js
+++ result/test/mock.test.js
@@ -4,7 +4,7 @@ describe('mock test 1', () => {
   it('unit test 1', () => {
     expect(true).to.be.true;
   });
-}); 
+});
 
 
 describe('mock test 2', () => {
@@ -25,5 +25,34 @@ describe('mock test 4', () => {
     expect(true).to.be.true;
   });
 });
+
+const expect = require('chai').expect;
+
+describe('mock test 1', () => {
+  it('unit test 1', () => {
+    expect(true).to.be.true;
+  });
+});
+
+
+describe('mock test 2', () => {
+  it('unit test 2', () => {
+    expect(true).to.be.true;
+  });
+});
+
+describe('mock test 3', () => {
+  it('unit test 3', () => {
+    expect(true).to.be.true;
+  });
+});
+
+
+describe('mock test 4', () => {
+  it('unit test 4', () => {
+    expect(true).to.be.true;
+  });
+});
+
 
 
