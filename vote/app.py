c1eed36fb74576123788db11efcb2fa2fbefa67e
pedro <user@gmail.com>
2024-11-23 21:51:38 -0300

    update file vote.app

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Chocolate")
-option_b = os.getenv('OPTION_B', "Vanilla")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 version = 'v1'
 
