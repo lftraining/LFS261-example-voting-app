93702d886f3baec4a31880784e386558d2956eb5
norahalr <norahnsrr@gmail.com>
2025-07-21 09:09:47 +0300

    adding Jenkinsfile for result app

---
+++ result/Jenkinsfile.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+    tools {
+        maven 'maven 3.9.8'
+    }
+    stages {
+        stage("build") {
+            steps {
+                echo 'Compiling worker app..'
+                dir('worker') {
+                    sh 'mvn compile'
+                }
+            }
+        }
+        stage("test") {
+            steps {
+                echo 'Running Unit Tests on worker app..'
+                dir('worker') {
+                    sh 'mvn clean test'
+                }
+            }
+        }
+        stage("package") {
+            steps {
+                echo 'Packaging worker app'
+                dir('worker') {
+                    sh 'mvn package -DskipTests'
+                }
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
+            echo 'Building multibranch pipeline for worker is completed..'
+        }
+    }
+}
