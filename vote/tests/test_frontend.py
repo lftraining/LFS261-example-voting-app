3f8fdf717db5a5c9ea7d1521f8c1eb6eb827fb4f
emmiduh <emmiduh@gmail.com>
2025-07-22 13:43:56 +0200

    consolidated pipe for instavote state

--- vote/tests/test_frontend.py
+++ vote/tests/test_frontend.py
@@ -1,7 +1,11 @@
-from vote.app import app
-from flask import url_for
+import sys
+import os
 import unittest
+from flask import url_for
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from vote.app import app
 
 class VoteUnitTest(unittest.TestCase):
 
